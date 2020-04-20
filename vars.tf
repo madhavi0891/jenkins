29c4a03e2c521e4a6a6fe702de4dc7c0ac8bf914
malthumkAR <54387347+malthumkAR@users.noreply.github.com>
2020-04-20 06:08:39 -0700

    Update vars.tf

--- vars.tf
+++ vars.tf
@@ -4,10 +4,10 @@
 
 variable "access_key" {
     description = "Your AWS Access Key"
-    default = " <copy-your-access-key-here>"
+    default = "AKIA6BVISBY5NV5AGMP7"
 }
 
 variable "secret_key" {
     description = "Your AWS Secret Key"
-    default = " <copy-your-access-key-here>"
+    default = "sc4n1wMBHWVtfjCEtxRZqzj4ihbFlz3YXse1QBaw"
 }
